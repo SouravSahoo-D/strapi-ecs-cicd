1a1e3ebf9db7b054359f7e4cb90127ae87a25f6f
SouravSahoo-D <sourav2.ranjan4@gmail.com>
2025-07-31 14:12:12 +0530

    Blue Green Deployment:2

--- terraform/main.tf
+++ terraform/main.tf
@@ -278,7 +278,7 @@ resource "aws_iam_role" "codedeploy_role" {
 
 resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
   role       = aws_iam_role.codedeploy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRoleForECS"
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
 }
 
 # CodeDeploy ECS Deployment Group
